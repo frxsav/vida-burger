9273fa8b8851d1561c384c2d552d814038f3105d
frxsav <keccofor@gmail.com>
2025-11-25 20:18:52 +0100

    second commit

--- src/ui/sections/Hero.jsx
+++ src/ui/sections/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
       className="min-h-screen bg-[url('/images/gym-hero.webp')] bg-no-repeat bg-cover">
       <div className="min-h-screen flex items-center justify-center dark:bg-secondary/20 bg-secondary-light/20">
         <p className="text-3xl dark:text-ctext-light text-ctext font-bold text-shadow-sm">
-          Testo di prova
+          Testo di prova 2
         </p>
       </div>
     </section>
